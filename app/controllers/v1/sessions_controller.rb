5f1cf4d8b2d567f7eb95865508b21317535a66e1
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-26 11:25:24 +0800

    Create user sessions view

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -5,10 +5,10 @@ class V1::SessionsController < ApplicationController
     @user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
     if @user &.valid_password?(params[:password])
       data = User.all.as_json.find { |user| user['username'] == @user.username }
-      token = JsonWebToken.encode(data)
-      render json: data.merge(token: token), status: :ok
+      @token = JsonWebToken.encode(data)
+      render :user, status: :ok
     else
-      render json: { "error": 'Cannot find' }
+      render json: { "error": 'Invalid credentials' }
     end
   end
 end

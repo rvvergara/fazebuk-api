f578cbd99893375867d37985d3af1cbfc0d230d5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-26 09:50:15 +0800

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :slug, presence: true
+  validates :slug, uniqueness: true
 end

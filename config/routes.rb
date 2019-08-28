f4bb6d24522b6a28f954abe445553fcae6aca85f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-28 14:40:19 +0800

    Add route to facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :sessions, only: [:create]
     resources :users, param: :username, only: [:show, :create, :update, :destroy]
+    get '/facebook/auth', to: 'facebook_authentications#create'
   end
 end

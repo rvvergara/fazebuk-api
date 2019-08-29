afe1bccaf75c01be940e2f0d93b8d57399edc402
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-29 12:40:32 +0800

    Fix route url for facebook authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :sessions, only: [:create]
     resources :users, param: :username, only: [:show, :create, :update, :destroy]
-    get '/facebook/auth', to: 'facebook_authentications#create'
+    get '/auth/facebook', to: 'facebook_authentications#create'
   end
 end

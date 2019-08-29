0c2a43bf5cba0a38c2e5e543998c37fae3f94441
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-29 16:51:32 +0800

    Create friendships controller and policy

---
+++ app/controllers/v1/friendships_controller.rb
@@ -0,0 +1,16 @@
+class V1::FriendshipsController < ApplicationController
+  before_action :pundit_user
+
+  def show
+    
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ app/policies/friendships_policy.rb
@@ -0,0 +1,7 @@
+class FriendshipsPolicy < ApplicationPolicy
+  class Scope < Scope    
+    def resolve
+      scope.all
+    end
+  end
+end

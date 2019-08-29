ea3f2dc505316ba2672cddf92fff3d6507d3d2f3
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-29 20:23:56 +0800

    Write policy spec for friendship destroy

--- spec/policies/friendships_policy_spec.rb
+++ spec/policies/friendships_policy_spec.rb
@@ -3,12 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe FriendshipPolicy, type: :policy do
-  let(:ryan) { create(:user, username: 'rvvergara') }
-  let(:anna) { create(:user, username: 'anna') }
-  let(:friendship) { create(:friendship, active_friend: ryan, passive_friend: anna) }
+  let(:kurt) { create(:user, username: 'kurt') }
+  let(:mary) { create(:user, username: 'mary') }
+  let(:friendship) { create(:friendship, active_friend: kurt, passive_friend: mary) }
 
-  context "anna confirms ryan's request" do
-    subject { FriendshipPolicy.new(anna, friendship) }
+  context 'mary can either confirm or reject the request' do
+    subject { FriendshipPolicy.new(mary, friendship) }
     it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+  end
+
+  context 'kurt can cancel the request' do
+    subject { FriendshipPolicy.new(kurt, friendship) }
+    it { is_expected.to permit_action(:destroy) }
   end
 end

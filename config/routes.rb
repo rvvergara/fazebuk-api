36ba47c4453a9c9183c5402d904d476290c04c6f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-29 16:48:13 +0800

    Add routes for friendships
    - Nest it under users
    - No route for new

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
     resources :sessions, only: [:create]
     resources :users, param: :username, only: [:show, :create, :update, :destroy]
     get '/auth/facebook', to: 'facebook_authentications#create'
+    resources :users, param: :username, only: [:show, :create, :update, :destroy] do
+      resources :friendships, only: [:index, :show, :create, :update, :destroy]
+    end
   end
 end

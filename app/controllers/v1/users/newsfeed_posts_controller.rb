9521bd733f75d20b085ab68f4533297b70e9bbb5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-04 19:13:04 +0800

    Add more safeguards to timeline_posts_controller
    - To make sure to show an error message when the page params is more
    than the max pages for timeline_posts results

--- app/controllers/v1/users/newsfeed_posts_controller.rb
+++ app/controllers/v1/users/newsfeed_posts_controller.rb
@@ -3,14 +3,14 @@
 class V1::Users::NewsfeedPostsController < ApplicationController
   before_action :pundit_user
   before_action do
-    @page = set_page
+    @page = set_page.to_i
   end
 
   def index
     records_per_page = 10
     posts_count = @current_user.newsfeed_posts.count
     @newsfeed_posts = @current_user.paginated_newsfeed_posts(@page, records_per_page)
-    if @page.to_i <= max_page(posts_count, records_per_page)
+    if @page <= max_page(posts_count, records_per_page)
       render :newsfeed_posts, status: :ok
     else
       render json: { message: 'No more newsfeed posts to show' }, status: :ok
--- app/controllers/v1/users/timeline_posts_controller.rb
+++ app/controllers/v1/users/timeline_posts_controller.rb
@@ -3,15 +3,20 @@
 class V1::Users::TimelinePostsController < ApplicationController
   before_action :pundit_user
   before_action do
-    @page = set_page
+    @page = set_page.to_i
   end
 
   def index
     @user = User.find_by(username: params[:user_username])
     if @user
       records_per_page = 10
-      @timeline_posts = @user.timeline_posts(@page, records_per_page)
-      render :timeline_posts, status: :ok
+      posts_count = @user.timeline_posts.count
+      @timeline_posts = @user.paginated_timeline_posts(@page, records_per_page)
+      if @page <= max_page(posts_count, records_per_page)
+        render :timeline_posts, status: :ok
+      else
+        render json: { message: 'No more timeline posts to show' }, status: :ok
+      end
     else
       render json: { message: 'Cannot find user' }, status: 404
     end

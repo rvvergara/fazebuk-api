acf79e139881df56d003e24c786fd4b399aa9686
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-05 18:24:38 +0800

    Add additional stats on friends related views
    - Create partial _friends_stats.json.jbuilder w/c contains info on the
    user's total friends and total shown on a page
    - Use this on friends.json.jbuilder

--- app/controllers/v1/users/friends_controller.rb
+++ app/controllers/v1/users/friends_controller.rb
@@ -3,13 +3,18 @@
 class V1::Users::FriendsController < ApplicationController
   before_action :pundit_user
   before_action do
-    @page = set_page
+    @page = set_page.to_i
   end
 
   def index
     @user = User.find_by(username: params[:user_username])
     records_per_page = 10
+    @friends_count = @user.friends.count
     @friends = @user.paginated_friends(@page, records_per_page)
-    render :friends, status: :ok
+    if @page <= max_page(@friends_count, records_per_page)
+      render :friends, status: :ok
+    else
+      render json: { message: 'No more friends to show' }
+    end
   end
 end
--- app/views/v1/users/friends/friends.json.jbuilder
+++ app/views/v1/users/friends/friends.json.jbuilder
@@ -4,3 +4,7 @@ json.friends_of @user.username
 json.friends @friends do |friend|
   json.partial! 'v1/shared/user', user: friend
 end
+json.partial! 'v1/shared/friends_stats',
+              page: @page,
+              friends_count: @friends.count,
+              total_friends_count: @friends_count

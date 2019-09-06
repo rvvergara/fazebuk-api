ae6ec57b5995908fb0e4cca0f1d7d9151e4bc029
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-06 17:02:57 +0800

    Add default scope to Comment (updated_at: ascending)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :replies, foreign_key: :commentable_id, class_name: 'Comment', as: :commentable, dependent: :destroy
 
+  default_scope { order(updated_at: :asc) }
+
   validates :body, presence: true
 end

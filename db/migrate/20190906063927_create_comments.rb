62b3479cecddd938abf5542850a26fa60f40c3de
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-06 15:39:58 +0800

    Change author_id col into commenter_id

--- db/migrate/20190906063927_create_comments.rb
+++ db/migrate/20190906063927_create_comments.rb
@@ -5,13 +5,13 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments, id: :uuid do |t|
       t.text :body
       t.uuid :commentable_id
-      t.uuid :author_id
+      t.uuid :commenter_id
       t.string :commentable_type
 
       t.timestamps
     end
     add_index :comments, :commentable_id
-    add_index :comments, :author_id
-    add_foreign_key :comments, :users, column: :author_id
+    add_index :comments, :commenter_id
+    add_foreign_key :comments, :users, column: :commenter_id
   end
 end

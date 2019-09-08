bdea665a9e585a34c08f71c6f2c8d76753dd2495
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-08 14:04:20 +0800

    Remove instance vars from newsfeed posts controller

--- app/controllers/v1/users/newsfeed_posts_controller.rb
+++ app/controllers/v1/users/newsfeed_posts_controller.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 class V1::Users::NewsfeedPostsController < ApplicationController
-  before_action do
-    @page = set_page.to_i
-  end
-
   def index
+    page = set_page
     records_per_page = 10
     posts_count = pundit_user.newsfeed_posts.count
-    @newsfeed_posts = pundit_user.paginated_newsfeed_posts(@page, records_per_page)
-    if set_max_in_page(@page, posts_count, records_per_page)
-      render :newsfeed_posts, status: :ok
+    newsfeed_posts = pundit_user.paginated_newsfeed_posts(page, records_per_page)
+    if set_max_in_page(page, posts_count, records_per_page)
+      render :newsfeed_posts,
+             locals: {
+               newsfeed_posts: newsfeed_posts,
+               page: page
+             },
+             status: :ok
     else
       render json: { message: 'No more newsfeed posts to show' }, status: :ok
     end

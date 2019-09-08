43e2fc35666782c5612378d2d08de73af183207c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-08 13:57:53 +0800

    Remove instance vars in mutual_friends_controller

--- app/controllers/v1/users/mutual_friends_controller.rb
+++ app/controllers/v1/users/mutual_friends_controller.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 class V1::Users::MutualFriendsController < ApplicationController
-  before_action do
-    @page = set_page.to_i
-  end
-
   def index
-    @user = User.find_by(username: params[:user_username])
+    page = set_page
+    user = User.find_by(username: params[:user_username])
     records_per_page = 10
-    @mutual_friends_count = pundit_user.mutual_friends_with(@user).count
-    @mutual_friends = pundit_user.paginated_mutual_friends_with(@user, @page, records_per_page)
+    mutual_friends_count = pundit_user.mutual_friends_with(user).count
+    mutual_friends = pundit_user.paginated_mutual_friends_with(user, page, records_per_page)
 
-    if set_max_in_page(@page, @mutual_friends_count, records_per_page)
-      render :mutual_friends, status: :ok
+    if set_max_in_page(page, mutual_friends_count, records_per_page)
+      render :mutual_friends,
+             locals: {
+               user: user,
+               mutual_friends: mutual_friends, mutual_friends_count: mutual_friends_count,
+               page: page
+             },
+             status: :ok
     else
       render json: { message: 'No more mutual friends to show' }
     end
--- app/views/v1/users/mutual_friends/mutual_friends.json.jbuilder
+++ app/views/v1/users/mutual_friends/mutual_friends.json.jbuilder
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-json.mutual_friends_with @user.username
-json.mutual_friends @mutual_friends
+json.mutual_friends_with user.username
+json.mutual_friends mutual_friends
 json.partial! 'v1/shared/friends_stats',
-              page: @page,
-              friends_count: @mutual_friends.count,
-              total_friends_count: @mutual_friends_count
+              page: page,
+              friends_count: mutual_friends.count,
+              total_friends_count: mutual_friends_count

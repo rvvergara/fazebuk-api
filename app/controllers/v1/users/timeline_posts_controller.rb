155479a64d842e8c589059d34a8dd0218e90fb54
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-08 14:07:17 +0800

    Remove instance vars from timeline_posts_controller

--- app/controllers/v1/users/timeline_posts_controller.rb
+++ app/controllers/v1/users/timeline_posts_controller.rb
@@ -2,19 +2,23 @@
 
 class V1::Users::TimelinePostsController < ApplicationController
   before_action :pundit_user
-  before_action do
-    @page = set_page.to_i
-  end
 
   def index
-    @user = User.find_by(username: params[:user_username])
-    if @user
+    page = set_page
+    user = User.find_by(username: params[:user_username])
+    if user
       records_per_page = 10
-      posts_count = @user.timeline_posts.count
-      @timeline_posts = @user.paginated_timeline_posts(@page, records_per_page)
+      posts_count = user.timeline_posts.count
+      timeline_posts = user.paginated_timeline_posts(page, records_per_page)
 
-      if set_max_in_page(@page, posts_count, records_per_page)
-        render :timeline_posts, status: :ok
+      if set_max_in_page(page, posts_count, records_per_page)
+        render :timeline_posts,
+               locals: {
+                 user: user,
+                 timeline_posts: timeline_posts,
+                 page: page
+               },
+               status: :ok
       else
         render json: { message: 'No more timeline posts to show' }, status: :ok
       end

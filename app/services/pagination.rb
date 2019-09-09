b269b3255a90e5b26abe5ff5b4576c3b5a3eccc7
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-09 09:09:02 +0800

    Make service files syntactically nice
    - Nest all methods inside class << self so that the method names don't
    need the self prefix anymore

--- app/services/pagination.rb
+++ app/services/pagination.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 class Pagination
-  def self.page(page_param)
-    page_param || '1'
-  end
+  class << self
+    def page(page_param)
+      page_param || '1'
+    end
 
-  def self.set_max_in_page(page, record_count, records_per_page)
-    page <= max_page(record_count, records_per_page)
-  end
+    def set_max_in_page(page, record_count, records_per_page)
+      page <= max_page(record_count, records_per_page)
+    end
 
-  def self.max_page(count, records_per_page)
-    (count / records_per_page.to_f).ceil
-  end
+    def max_page(count, records_per_page)
+      (count / records_per_page.to_f).ceil
+    end
 
-  def self.offset(page, per_page)
-    (page.to_i - 1) * per_page
+    def offset(page, per_page)
+      (page.to_i - 1) * per_page
+    end
   end
 end

d0f2b7737081e858328cb1caf1dfab934268eed5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-09 18:13:24 +0800

    Create Likes factory
    - Use trait to create polymorphic association with post and
    comments/replies

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -2,8 +2,16 @@
 
 FactoryBot.define do
   factory :like do
-    liker_id { '' }
-    likeable_type { 'MyString' }
-    likeable_id { '' }
+    trait :for_post do
+      association(:likeable, factory: :post)
+    end
+
+    trait :for_post_comment do
+      association(:likeable, factory: :post_comment)
+    end
+
+    trait :for_comment_reply do
+      association(:likeable, factory: :comment_reply)
+    end
   end
 end

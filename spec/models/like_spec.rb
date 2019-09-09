da9338b818d09566c393034c814c2aceb864115f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-09 20:55:26 +0800

    Add validation to like and write specs
    - Add private method that adds an error if liker and likeable
    combination is a duplicate of an existing record
    
    - Use before_validation hook for this
    
    - Write validation specs for like_spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:mark) { create(:male_user, username: 'mark') }
+  let(:luke) { create(:male_user, username: 'luke') }
+  let(:post) { create(:post, author: mark, postable: luke) }
+  let(:comment) { create(:post_comment, commenter: luke, commentable: post) }
+  let!(:like) { create(:like, :for_post, likeable: post, liker: luke) }
+
+  describe 'validation' do
+    context 'duplicate like' do
+      it 'is invalid' do
+        duplicate = build(:like, :for_post, liker: luke, likeable: post)
+        duplicate.valid?
+        expect(duplicate.errors['liker']).to include('cannot like the Post twice')
+      end
+
+      context 'unique like' do
+        it 'is valid' do
+          comment_like = build(:like, :for_post_comment, liker: mark, likeable: comment)
+          expect(comment_like).to be_valid
+        end
+      end
+    end
+  end
 end

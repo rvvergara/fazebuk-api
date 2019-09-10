a1acf08ef921523de10c998a6cbebae365ad1880
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-10 21:27:31 +0800

    Use traits on friendship
    To differentiate between a confirmed friendship and a pending
    Also use alias request

--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :friendship do
+  factory :friendship, aliases: [:request] do
     active_friend
     passive_friend
+
+    trait :confirmed do
+      confirmed { true }
+    end
   end
 end

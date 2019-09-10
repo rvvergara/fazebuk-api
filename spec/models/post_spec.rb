19d973b05e71081534c18638b2053b8eefa78f63
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-10 12:04:32 +0800

    Write post spec for like_id inherited method

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe 'validation' do
-    let(:charlie) { create(:male_user, username: 'charlie') }
-    let(:kyle) { create(:male_user, username: 'kyle') }
-    let(:post) { build(:post, author: charlie, postable: kyle) }
+  let(:charlie) { create(:male_user, username: 'charlie') }
+  let(:kyle) { create(:male_user, username: 'kyle') }
+  let(:post) { build(:post, author: charlie, postable: kyle) }
 
+  describe 'validation' do
     context 'content present in post' do
       it 'is valid' do
         expect(post).to be_valid
@@ -23,6 +23,22 @@ RSpec.describe Post, type: :model do
     end
   end
 
+  describe 'inherited #like_id' do
+    context 'user has liked the post' do
+      before { post.save }
+
+      it 'returns like id' do
+        like = create(:like, :for_post, likeable: post, liker: kyle)
+        expect(post.like_id(kyle)).to eq(like.id)
+      end
+    end
+    context 'user has not liked the post' do
+      it 'returns nil' do
+        expect(post.like_id(kyle)).to be(nil)
+      end
+    end
+  end
+
   describe 'associations' do
     it {
       should belong_to(:postable)
@@ -34,6 +50,12 @@ RSpec.describe Post, type: :model do
     }
     it {
       should have_many(:comments)
+        .with_foreign_key(:commentable_id)
+        .dependent(:destroy)
+    }
+    it {
+      should have_many(:likes)
+        .with_foreign_key(:likeable_id)
         .dependent(:destroy)
     }
   end

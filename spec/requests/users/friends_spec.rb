0781c24fe4d048e1225826599c39a4ae21964aad
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-12 12:48:08 +0800

    Refactor friends_spec and mutual_friends_spec
    To include unauthenticated requests
    To make use of helper methods
    To provide complete coverage

--- spec/requests/users/friends_spec.rb
+++ spec/requests/users/friends_spec.rb
@@ -3,24 +3,63 @@
 require 'rails_helper'
 
 RSpec.describe 'Users::Friends', type: :request do
-  let(:harry) { create(:user, :male, first_name: 'harry') }
-  let(:hermione) { create(:user, :female, first_name: 'Hermione') }
-  let(:goku) { create(:user, :male, first_name: 'Goku') }
+  let(:harry) { create(:user, :male, first_name: 'Harry') }
+  let!(:friends) do
+    8.times do
+      user = create(:user, :male, username: generate(:username))
+      create(:friendship, :confirmed, active_friend: user, passive_friend: harry)
+    end
+
+    7.times do
+      user = create(:user, :male, username: generate(:username))
+      create(:friendship, :confirmed, active_friend: harry, passive_friend: user)
+    end
+  end
+
+  def friends_route(username, page = nil)
+    page_param = page ? "?page=#{page}" : nil
+    "/v1/users/#{username}/friends#{page_param}"
+  end
+
+  describe 'unauthenticated user request' do
+    it {
+      get friends_route(harry.username)
+      expect(response).to have_http_status(:unauthorized)
+    }
+  end
 
   describe 'GET /v1/users/:user_username/friends' do
-    before do
-      [harry, hermione].each do |friend|
-        create(:friendship, active_friend: goku, passive_friend: friend, confirmed: true)
+    let!(:login) { login_as(harry) }
+
+    context 'request without specified page' do
+      it 'responds with first page (10 friends)' do
+        get friends_route(harry.username),
+            headers: authorization_header
+
+        expect(response).to have_http_status(:ok)
+        expect(json_response.keys).to match(friends_response_keys)
+        expect(json_response['total_shown_on_page']).to be(10)
       end
     end
-    context 'goku logs on to check his friends' do
-      it 'gives him an array of json data of his friends' do
-        login_as(goku)
-        get "/v1/users/#{goku.username}/friends",
-            headers: { "Authorization": "Bearer #{user_token}" }
-        json_response = JSON.parse(response.body)
-        expect(json_response['friends'].size).to be(2)
+
+    context 'request for page 2' do
+      it 'responds with second page (5 friends)' do
+        get friends_route(harry.username, 2),
+            headers: authorization_header
+
         expect(response).to have_http_status(:ok)
+        expect(json_response.keys).to match(friends_response_keys)
+        expect(json_response['total_shown_on_page']).to be(5)
+      end
+    end
+
+    context 'request for page 3' do
+      it 'sends a no more to display message' do
+        get friends_route(harry.username, 3),
+            headers: authorization_header
+
+        expect(response).to have_http_status(:ok)
+        expect(json_response['message']).to match('No more friends to show')
       end
     end
   end
--- spec/requests/users/mutual_friends_spec.rb
+++ spec/requests/users/mutual_friends_spec.rb
@@ -3,25 +3,66 @@
 require 'rails_helper'
 
 RSpec.describe 'Users::Friends', type: :request do
-  let(:harry) { create(:user, :male, first_name: 'harry') }
-  let(:hermione) { create(:user, :female, first_name: 'Hermione') }
-  let(:goku) { create(:user, :male, first_name: 'Goku') }
+  let(:harry) { create(:user, :male, first_name: 'Harry') }
+  let(:gerry) { create(:user, :male, first_name: 'Gerry') }
+  let!(:friends) do
+    8.times do
+      user = create(:user, :male, username: generate(:username))
+      create(:friendship, :confirmed, active_friend: user, passive_friend: harry)
+      create(:friendship, :confirmed, active_friend: gerry, passive_friend: user)
+    end
+
+    7.times do
+      user = create(:user, :male, username: generate(:username))
+      create(:friendship, :confirmed, active_friend: harry, passive_friend: user)
+      create(:friendship, :confirmed, active_friend: user, passive_friend: gerry)
+    end
+  end
+
+  def mutual_friends_route(username, page = nil)
+    page_param = page ? "?page=#{page}" : nil
+    "/v1/users/#{username}/mutual_friends#{page_param}"
+  end
+
+  describe 'unauthenticated user request' do
+    it {
+      get mutual_friends_route(gerry.username)
+      expect(response).to have_http_status(:unauthorized)
+    }
+  end
 
   describe 'GET /v1/users/:user_username/friends' do
-    before do
-      [harry, hermione].each do |friend|
-        create(:friendship, active_friend: goku, passive_friend: friend, confirmed: true)
+    let!(:login) { login_as(harry) }
+
+    context 'request without specified page' do
+      it 'responds with first page (10 friends)' do
+        get mutual_friends_route(gerry.username),
+            headers: authorization_header
+
+        expect(response).to have_http_status(:ok)
+        expect(json_response.keys).to match(mutual_friends_response_keys)
+        expect(json_response['total_shown_on_page']).to be(10)
       end
     end
-    context 'harry visits his mutual friends with hermione page' do
-      it 'returns an array w/ goku in it' do
-        login_as(harry)
-        get "/v1/users/#{hermione.username}/mutual_friends",
-            headers: { "Authorization": "Bearer #{user_token}" }
 
-        json_response = JSON.parse(response.body)
-        expect(json_response['mutual_friends'].size).to be(1)
+    context 'request for page 2' do
+      it 'responds with second page (5 friends)' do
+        get mutual_friends_route(gerry.username, 2),
+            headers: authorization_header
+
         expect(response).to have_http_status(:ok)
+        expect(json_response.keys).to match(mutual_friends_response_keys)
+        expect(json_response['total_shown_on_page']).to be(5)
+      end
+    end
+
+    context 'request for page 3' do
+      it 'sends a no more to display message' do
+        get mutual_friends_route(gerry.username, 3),
+            headers: authorization_header
+
+        expect(response).to have_http_status(:ok)
+        expect(json_response['message']).to match('No more mutual friends to show')
       end
     end
   end

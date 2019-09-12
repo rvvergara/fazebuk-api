68572fe285c19640b83cc3c352d9a65bdfdb0f3b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-12 11:07:07 +0800

    Fix variable names in users/friends_controller
    As well as the corresponding variable names in friends.json.jbuilder and
    friends_stats partial
    
    In order to make the data more understandable in the controller

--- app/controllers/v1/users/friends_controller.rb
+++ app/controllers/v1/users/friends_controller.rb
@@ -7,16 +7,17 @@ class V1::Users::FriendsController < ApplicationController
     page = set_page
     user = User.find_by(username: params[:user_username])
     records_per_page = 10
-    friends_count = user.friends.count
-    friends = user.paginated_friends(page, records_per_page)
+    all_friends = user.friends
+    total_friends_count = all_friends.count
+    displayed_friends = user.paginated_friends(page, records_per_page)
 
-    if set_max_in_page(page, friends_count, records_per_page)
+    if set_max_in_page(page, total_friends_count, records_per_page)
       render :friends,
              locals: {
                user: user,
-               friends: friends,
+               displayed_friends: displayed_friends,
                page: page,
-               friends_count: friends_count
+               total_friends_count: total_friends_count
              },
              status: :ok
     else
--- app/views/v1/users/friends/friends.json.jbuilder
+++ app/views/v1/users/friends/friends.json.jbuilder
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 json.friends_of user.username
-json.friends friends do |friend|
+json.displayed_friends displayed_friends do |friend|
   json.partial! 'v1/shared/user', user: friend
 end
 json.partial! 'v1/shared/friends_stats',
               page: page,
-              friends_count: friends.count,
-              total_friends_count: friends_count
+              displayed_friends_count: displayed_friends.count,
+              total_friends_count: total_friends_count

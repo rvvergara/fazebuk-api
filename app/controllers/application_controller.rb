677b90e33834c0018be653eb3d8d4104fde6c20e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-12 14:08:34 +0800

    Add and modify methods in application_controller
    1. Rename render_error to find_error
    2. Define process_error method which handles errors coming from failure
    to process resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,7 +54,11 @@ class ApplicationController < ActionController::API
     }
   end
 
-  def render_error(resource_type)
+  def find_error(resource_type)
     render json: { message: "Cannot find #{resource_type}" }, status: 404
+  end
+
+  def process_error(resource, message)
+    render json: { message: message, errors: resource.errors }, status: :unprocessable_entity
   end
 end

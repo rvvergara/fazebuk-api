9264c73d1601ec9ee2dff4da1c6a0ec8ced8d253
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-16 13:00:51 +0800

    Add with_profile_pic and with_cover_pic traits
    - To users factory
    - Use Rack::Test::UploadFile to use test image in spec/support/assets

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user, aliases: %i[author commenter liker postable active_friend passive_friend] do
+  factory :user,
+          aliases: %i[author commenter liker postable active_friend passive_friend] do
     trait :male do
       gender { 'male' }
       first_name { Faker::Name.male_first_name }
@@ -14,6 +15,18 @@ FactoryBot.define do
 
     trait :invalid do
       username { first_name }
+    end
+
+    trait :with_profile_images do
+      profile_images do
+        Rack::Test::UploadedFile.new('spec/support/assets/kayi.jpg', 'image/jpg')
+      end
+    end
+
+    trait :with_cover_images do
+      cover_images do
+        Rack::Test::UploadedFile.new('spec/support/assets/kayi.jpg', 'image/jpg')
+      end
     end
 
     last_name { Faker::Name.last_name }

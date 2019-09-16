b59bcf0100851c911fd033b4e1a90cbaeb5677e4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-16 10:03:56 +0800

    Add profile_pic and cover_pic columns to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_014221) do
+ActiveRecord::Schema.define(version: 2019_09_16_015045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 2019_09_16_014221) do
     t.string "middle_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_pic"
+    t.string "cover_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true

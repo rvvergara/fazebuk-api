3ff705e5afc3b5d45b26bca46a9ec140d34cdb17
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-17 16:18:43 +0800

    Add active_storage association to post model
    has_many_attached pics with dependent: :purge

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :commentable_id, as: :commentable, dependent: :destroy
   has_many :likes, foreign_key: :likeable_id, as: :likeable, dependent: :destroy
+  has_many_attached :pics, dependent: :purge
 
   scope :order_created, -> { order(created_at: :desc) }
 
